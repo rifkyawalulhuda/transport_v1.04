60a37f7dba800262858faa69b324578af523e685
Rifky Awalul Huda <rifkyawalulhuda@gmail.com>
2026-07-18 21:02:01 +0700

    fix: remove IF NOT EXISTS from migration (MySQL version compatibility)

--- node_backend/db/migrations/20260717000008_add_gps_cache_to_truck.sql
+++ node_backend/db/migrations/20260717000008_add_gps_cache_to_truck.sql
@@ -1,15 +1,15 @@
 -- migrate:up
 ALTER TABLE truck
-  ADD COLUMN IF NOT EXISTS last_lat       DOUBLE        NULL DEFAULT NULL,
-  ADD COLUMN IF NOT EXISTS last_lng       DOUBLE        NULL DEFAULT NULL,
-  ADD COLUMN IF NOT EXISTS last_address   VARCHAR(255)  NULL DEFAULT NULL,
-  ADD COLUMN IF NOT EXISTS last_gps_time  DATETIME      NULL DEFAULT NULL;
+  ADD COLUMN last_lat       DOUBLE        NULL DEFAULT NULL,
+  ADD COLUMN last_lng       DOUBLE        NULL DEFAULT NULL,
+  ADD COLUMN last_address   VARCHAR(255)  NULL DEFAULT NULL,
+  ADD COLUMN last_gps_time  DATETIME      NULL DEFAULT NULL;
 
 -- migrate:down
 ALTER TABLE truck
-  DROP COLUMN IF EXISTS last_lat,
-  DROP COLUMN IF EXISTS last_lng,
-  DROP COLUMN IF EXISTS last_address,
-  DROP COLUMN IF EXISTS last_gps_time;
+  DROP COLUMN last_lat,
+  DROP COLUMN last_lng,
+  DROP COLUMN last_address,
+  DROP COLUMN last_gps_time;
 
 
